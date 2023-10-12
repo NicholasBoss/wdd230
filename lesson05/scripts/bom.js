e51f97d8d3e3667823d4053539d0199b50379746
NicholasBoss <nicholasboss0914@gmail.com>
2023-10-12 15:30:24 -0600

    fixed javascript in Book of Mormon file

--- lesson05/scripts/bom.js
+++ lesson05/scripts/bom.js
@@ -3,17 +3,19 @@ const button = document.querySelector('button');
 const list = document.querySelector('#list');
 
 button.addEventListener('click', () => {
-    if (input.value != '') {
-        const li = document.createElement('li');
-        const deleteButton = document.createElement('button'); 
-        li.textContent = input.value;
-        deleteButton.textContent = '❌';
-        li.append(deleteButton);
-        list.append(li);
-        deleteButton.addEventListener('click', () => {
-            list.removeChild(li);
-            input.focus();
-        })
-        input.value = '';
-}
+    if (input.value == '') {
+        input.focus();
+        return;
+    }
+    let li = document.createElement('li');
+    let deleteButton = document.createElement('button'); 
+    li.textContent = input.value;
+    deleteButton.textContent = '❌';
+    li.appendChild(deleteButton);
+    list.append(li);
+    deleteButton.addEventListener('click', () => {
+        li.remove();
+    })
+    input.focus();
+    input.value = '';
 });
