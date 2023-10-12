6050142eece65c5dd9add6f57dca9542d26d402a
NicholasBoss <nicholasboss0914@gmail.com>
2023-10-12 17:36:34 -0600

    Fixed Commerce Dark Mode

--- chamber/scripts/darkmode.js
+++ chamber/scripts/darkmode.js
@@ -5,6 +5,9 @@ const h3 = document.getElementsByTagName("h3");
 modeButton.addEventListener("click", () => {
 	if (modeButton.textContent.includes("🕶️")) {
 		main.style.background = "#000";
+        document.documentElement.style.setProperty('--ham-color', 'white');
+        document.documentElement.style.setProperty('--accent3-color', '#fff')
+        document.documentElement.style.setProperty('--secondary-color', '#000');
 		main.style.color = "#fff";
         Array.from(h3).map((h3) => {
             h3.style.color = "#000";
@@ -12,6 +15,9 @@ modeButton.addEventListener("click", () => {
 		modeButton.textContent = "🔆";
 	} else {
 		main.style.background = "#eee";
+        document.documentElement.style.setProperty('--accent3-color', '#000');
+        document.documentElement.style.setProperty('--secondary-color', '#D5A021');
+        document.documentElement.style.setProperty('--ham-color', '#d5a021');
 		main.style.color = "#000";
 		modeButton.textContent = "🕶️";
 	}
