68aefbdb44e35bc7c4702d14d7e7de3530988ff4
NicholasBoss <nicholasboss0914@gmail.com>
2023-11-03 07:14:58 -0600

    practicing and implementing code from the learning exercises during week 8

---
+++ scripts/form.js
@@ -0,0 +1,18 @@
+const rating = document.getElementById('rating')
+rating.addEventListener('change', () => {
+    document.getElementById('currentrating').innerHTML = rating.value
+})
+
+const confirm = document.getElementById('confirm')
+confirm.addEventListener('blur', () => {
+    const password = document.getElementById('password')
+    if (password.value != confirm.value) {
+        document.getElementById('message').innerHTML = 'Passwords do not match'
+        password.value=''
+        confirm.value=''
+        password.focus()
+    }
+    else {
+        document.getElementById('message').innerHTML = ''
+    }
+})
