c730bd6fc6139e47f48f076ea63d594c6386f944
NicholasBoss <nicholasboss0914@gmail.com>
2023-11-20 16:26:11 -0700

    fixed hiding of search bar when nav is opened in mobile view.

--- chamber/scripts/search.js
+++ chamber/scripts/search.js
@@ -3,12 +3,12 @@ const overlay = document.querySelector('.overlay');
 
 function openSearch() {
     overlay.style.display = 'block';
-    searchBtn.style.display = 'none';
+    searchBtn.classList.toggle('hide');
 }
 
 function closeSearch() {
     overlay.style.display = 'none';
-    searchBtn.style.display = 'block';
+    searchBtn.classList.toggle('hide');
 }
 
 searchBtn.addEventListener('click', () => {
