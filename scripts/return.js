3353e5b25fdc5ba1998a634dba74409c8a44e36a
NicholasBoss <nicholasboss0914@gmail.com>
2023-11-20 15:09:29 -0700

    fixing javascript for return.js

--- scripts/return.js
+++ scripts/return.js
@@ -1,5 +1,10 @@
-const returnButton = document.querySelector('#return-button');
-
-returnButton.addEventListener('click', () => {
-    window.location.href = './chamber/index.html';
-});
+let count = 5;
+document.querySelector('.count-down').textContent = count;
+let countdown = setInterval(function() {
+    count--;
+    document.querySelector('.count-down').textContent = count;
+    if (count === 0) {
+        clearInterval(countdown);
+        window.location.href = "chamber/index.html";
+    }
+}, 1000);
